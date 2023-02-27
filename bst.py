4299687455cfb80b4071438c6f5ef33251b1f47e
Ankan Dash <ankandash18@gmail.com>
2023-02-27 14:14:44 -0500

    initial commit

---
+++ bst.py
@@ -0,0 +1,11 @@
+# Binary search tree
+
+class binarySearchTree():
+    def __init__(self, value):
+        self.value = None
+        self.left = None
+        self.right = None
+
+
+    def inset(self, value):
+        
