b55cedb2267f35deef66fd53b592959308bb0054
Ankan Dash <ankandash18@gmail.com>
2023-02-28 14:32:30 -0500

    bst iterative

---
+++ bst_iterative.py
@@ -0,0 +1,34 @@
+class BST:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+    # average: O(log(n)) time and O(1) space as it is iterative
+    def insert(self, value):
+        currentNode = self
+        while True:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
+
+    def contains(self, value):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return True
+        return False
